4dfc01f826e458c3f089fe008c5efc5d60bd719f
lnsdlhfem <sp1018@foxmail.com>
2017-09-21 16:22:39 +0800

    configuration

---
+++ src/main/java/com/lnsdlhfem/ApplicationConfiguration.java
@@ -0,0 +1,16 @@
+package com.lnsdlhfem;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 应用配置类
+ *
+ * @ClassName: ApplicationConfiguration
+ * @author: lnsdlhfem
+ * @date: 2017/9/21 16:01
+ */
+@Configuration
+public class ApplicationConfiguration {
+
+
+}
